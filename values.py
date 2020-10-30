cee148de6b0e187251a67c01a334a9aeb6ff999f
Abacination <noodles5733@live.com>
2020-10-30 13:44:40 -0700

    new fpv2

--- values.py
+++ values.py
@@ -247,11 +247,13 @@ FW_VERSIONS = {
       b'\xb5\"@p\a',
       b'\xab*@r\a',
       b'\xa0\"@\x80\a',
+      b'\x01EB4G700C\x00\x00\x00\x00\x00\x00\x00\x00',
     ],
     (Ecu.transmission, 0x7e1, None): [
       b'\xbc\xf2\x00\x81\x00',
       b'\xbe\xf2\x00p\x00',
       b'\xbf\xf2\x00\x80\x00',
+      b'\xf1\x00\xa4\x10@',
     ],
   },
   CAR.OUTBACK_PREGLOBAL: {
